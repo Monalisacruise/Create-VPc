edc312b17e9a1c844f26d7a280c2f20be772f0e9
Monalisacruise <monalisacruise307@gmail.com>
2025-11-03 15:55:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "vpc" {
 
   name = "vpc-dev"
   cidr = "10.0.0.0/16"
-  azs  = ["us-east-1a", "us-east-1b"]
+  azs  = ["ap-south-1a", "ap-south-1b"]
 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
