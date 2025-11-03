401fa0624ef33ecb6f94af47d596996ad5222750
Monalisacruise <monalisacruise307@gmail.com>
2025-11-03 15:45:47 +0530

    Update and rename main_vpc.tf to variablee.tf

--- variablee.tf
+++ variablee.tf
@@ -1,8 +1,5 @@
-# Input Variables
-
-# AWS Region
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
-  type = string
-  default = "us-east-1"  
+  type        = string
+  default     = "ap-south-1"
 }
